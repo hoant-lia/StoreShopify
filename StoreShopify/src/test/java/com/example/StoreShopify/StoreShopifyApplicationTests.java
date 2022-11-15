5d0d19cdfada47732f5ede0f082b7ba3ef11905c
hoant-lia <hoant.ptit@gmail.com>
2022-11-15 11:38:35 +0700

    Create project store shopify

---
+++ StoreShopify/src/test/java/com/example/StoreShopify/StoreShopifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.StoreShopify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreShopifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
